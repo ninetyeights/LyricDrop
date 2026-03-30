4917854324ed4728bdfd40341efecc6744aa170f
Chester Jin <clover1998love@gmail.com>
2026-03-30 19:02:07 -0400

    Initial Commit

---
+++ LyricDrop/LyricDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  LyricDropApp.swift
+//  LyricDrop
+//
+//  Created by Chester Jin on 2026-03-30.
+//
+
+import SwiftUI
+
+@main
+struct LyricDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
